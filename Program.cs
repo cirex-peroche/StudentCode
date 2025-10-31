f1bd4f7f313dfe0d27f01ab495d2a074ff5c3cba
cirex-peroche <cirex_peroche@yahoo.com>
2025-10-31 09:55:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using StudentCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SchoolContext())
+        {
+            var student = new Student() { StudentName = "John Doe" };
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            Console.WriteLine("Student added successfully!");
+        }
+
+        Console.ReadLine();
+    }
+}
